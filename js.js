a8e8a7fd4b53a1835d03506b77678af47aaee391
shivani718 <59058859+shivani718@users.noreply.github.com>
2021-07-19 00:09:15 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,13 @@
+function login()
+    {
+        var userName=document.getElementById("username");
+        var password=document.getElementById("password");
+        if(userName.value=="admin" &&password.value =="admin")
+        {
+            location.href ="home.html";
+        }
+        else
+        {
+            alert("wrong username / password");
+        }
+    }
